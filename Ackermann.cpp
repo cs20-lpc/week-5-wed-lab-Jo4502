d78ed42229afdd8b38a414f94995ac61527962d1
Jo4502 <jcamador@zonemail.clpccd.edu>
2025-09-20 22:21:48 -0700

    Add files via upload

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -4,6 +4,15 @@ using namespace std;
 // Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
  // TODO: Complete the code.
+    if (m == 0) {               // base case 1
+        return n + 1;
+    }
+    else if (n == 0) {          //base case 2
+        return ackermann( m - 1, 1);
+    }
+    else {                      // recursive case m>0 and n>0
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
 }
 
 int main() {
@@ -13,6 +22,11 @@ int main() {
 
     cout << "Ackermann(" << m << ", " << n << ") = " 
          << ackermann(m, n) << endl;
+
+    // try A(3,4)
+    cout << "Ackermann(3,4) = " << ackermann(3, 4) << endl;
+
+
 
     return 0;
 }
